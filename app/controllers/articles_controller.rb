6ffdc6424bd237e5898dcaf6411fb5bbad32e68c
kennedy <kimaniken2001@gmail.com>
2023-06-29 14:59:11 +0300

    completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,15 @@
+# articles_controller.rb
 class ArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  def index
-    articles = Article.all.includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
-  end
-
   def show
     article = Article.find(params[:id])
+    increment_page_views
     render json: article
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def increment_page_views
+    session[:page_views] ||= 0
+    session[:page_views] += 1
   end
-
 end
